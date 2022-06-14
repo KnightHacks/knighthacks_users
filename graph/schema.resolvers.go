688a45a36a0a53deee168760534f0017aedc4bd0
Warren Snipes <contact@warrensnipes.dev>
2022-06-14 02:31:04 -0400

    added documentation for Login & Register

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -16,6 +16,7 @@ import (
 )
 
 func (r *mutationResolver) Register(ctx context.Context, provider model.Provider, encryptedOauthAccessToken string, input model.NewUser) (*model.User, error) {
+	// convert model.Provider to auth.Provider, TODO: merge them with gqlgen magic
 	var authProvider auth.Provider
 	if provider == model.ProviderGithub {
 		_ = auth.GitHubAuthProvider
@@ -24,19 +25,22 @@ func (r *mutationResolver) Register(ctx context.Context, provider model.Provider
 	} else {
 		panic("new provider not fully implemented")
 	}
+	// Decode the encrypted OAuth AccessToken from base64
 	b, err := base64.URLEncoding.DecodeString(encryptedOauthAccessToken)
 	if err != nil {
 		return nil, err
 	}
+	// Decrypt the decoded access token using AES-256 decryption
 	accessToken, err := r.Auth.DecryptAccessToken(string(b))
 	if err != nil {
 		return nil, err
 	}
+	// Using the access token retrieve the OAuth provided UID of the user
 	uid, err := r.Auth.GetUID(ctx, authProvider, string(accessToken))
 	if err != nil {
 		return nil, err
 	}
-
+	// Create the user using the UID to check against duplicate accounts
 	user, err := r.Repository.CreateUser(ctx, &model.OAuth{UID: uid, Provider: provider}, &input)
 	if err != nil {
 		// TODO: Possibly do some error handling hear to filter sql errors out
@@ -63,6 +67,7 @@ func (r *queryResolver) GetAuthRedirectLink(ctx context.Context, provider model.
 }
 
 func (r *queryResolver) Login(ctx context.Context, provider model.Provider, code string) (*model.LoginPayload, error) {
+	// convert model.Provider to auth.Provider, TODO: merge them with gqlgen magic
 	var authProvider auth.Provider
 	if provider == model.ProviderGithub {
 		_ = auth.GitHubAuthProvider
@@ -71,33 +76,42 @@ func (r *queryResolver) Login(ctx context.Context, provider model.Provider, code
 	} else {
 		panic("new provider not fully implemented")
 	}
+	// Using the OAuth code provided exchange the code for an access token
 	token, err := r.Auth.ExchangeCode(ctx, authProvider, code)
 	if err != nil {
 		return nil, err
 	}
 	if !token.Valid() {
+		// this shouldn't happen unless there was man-in-the-middle tampering to the HTTP request involved
 		return nil, errors.New("auth token not valid, nice try hacker")
 	}
 	log.Printf("accessToken=%s, refreshToken=%s, type=%s, expiry=%s\n", token.AccessToken, token.RefreshToken, token.Type(), token.Expiry)
+	// Get the user by their OAuth ID, if the user == nil then the user hasn't created an account yet, but will using the Register function
 	user, err := r.Repository.GetUserByOAuthUID(ctx, token.AccessToken)
 	if err != nil {
 		return nil, err
 	}
-	encryptAccessTokenBytes := r.Auth.EncryptAccessToken(token.AccessToken)
-	log.Printf("bytes=%v\n", encryptAccessTokenBytes)
-	encryptAccessToken := base64.URLEncoding.EncodeToString(encryptAccessTokenBytes)
-	log.Printf("string=%v\n", encryptAccessToken)
-	payload := model.LoginPayload{
-		EncryptedOAuthAccessToken: &encryptAccessToken,
-	}
-
+	payload := model.LoginPayload{}
 	if user != nil {
+		// Set the user since they exist
 		payload.User = user
+		payload.AccountExists = true
+		// TODO: Implement JWT
+	} else {
+		// Using AES-256 encryption, encrypt the access token to protect against packet sniffing
+		encryptAccessTokenBytes := r.Auth.EncryptAccessToken(token.AccessToken)
+		log.Printf("bytes=%v\n", encryptAccessTokenBytes)
+
+		// Using base64 encoding, encode the access token to be able to be sent using alphanumeric character over HTTP
+		encodedAccessToken := base64.URLEncoding.EncodeToString(encryptAccessTokenBytes)
+		log.Printf("string=%v\n", encodedAccessToken)
+
+		payload.EncryptedOAuthAccessToken = &encodedAccessToken
 	}
 
-	// TODO: Implement JWT
-
-	payload.AccountExists = payload.User != nil
+	// The idea behind the last if statement is to return the user if it exists,
+	// if the user does not exist then we should supply the user with the encodedAccessToken
+	// to be able to Register an account
 	return &payload, nil
 }
 
