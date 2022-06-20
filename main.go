c4700f9c0ad990ba5873c8001034612d3c15405e
LockedThread <contact@warrensnipes.dev>
2022-06-20 01:45:31 -0400

    Revert "Revert "Version 1.0.0""

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/KnightHacks/knighthacks_shared/auth"
+	"github.com/KnightHacks/knighthacks_shared/models"
+	"github.com/KnightHacks/knighthacks_shared/pagination"
+	"github.com/KnightHacks/knighthacks_shared/utils"
+	"github.com/KnightHacks/knighthacks_users/graph/model"
 	"github.com/KnightHacks/knighthacks_users/repository"
+	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -30,7 +35,7 @@ func main() {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 
-	oauthConfigMap := map[auth.Provider]oauth2.Config{
+	oauthConfigMap := map[models.Provider]oauth2.Config{
 		//TODO: implement gmail auth, github is priority
 		//auth.GmailAuthProvider: {
 		//	ClientID:     "",
@@ -39,7 +44,7 @@ func main() {
 		//	RedirectURL:  "",
 		//	Scopes:       nil,
 		//},
-		auth.GitHubAuthProvider: {
+		models.ProviderGithub: {
 			ClientID:     getEnvOrDie("OAUTH_GITHUB_CLIENT_ID"),
 			ClientSecret: getEnvOrDie("OAUTH_GITHUB_CLIENT_SECRET"),
 			RedirectURL:  getEnvOrDie("OAUTH_GITHUB_REDIRECT_URL"),
@@ -55,20 +60,54 @@ func main() {
 	if err != nil {
 		log.Fatalf("An error occured when trying to create an instance of Auth: %s\n", err)
 	}
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{
+	ginRouter := gin.Default()
+	ginRouter.Use(auth.AuthContextMiddleware(newAuth))
+	ginRouter.Use(utils.GinContextMiddleware())
+
+	ginRouter.POST("/query", graphqlHandler(newAuth, pool))
+	ginRouter.GET("/", playgroundHandler())
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatalln(ginRouter.Run())
+}
+
+func graphqlHandler(newAuth *auth.Auth, pool *pgxpool.Pool) gin.HandlerFunc {
+	hasRoleDirective := auth.HasRoleDirective{GetUserId: func(ctx context.Context, obj interface{}) (string, error) {
+		switch t := obj.(type) {
+		case *model.User:
+			return t.ID, nil
+		default:
+			// shouldn't happen, you must implement the new object with the ID field
+			return "", errors.New("this shouldn't happen")
+		}
+	}}
+
+	config := generated.Config{
 		Resolvers: &graph.Resolver{
 			Repository: repository.NewDatabaseRepository(pool),
 			Auth:       *newAuth,
 		},
-	}))
+		Directives: generated.DirectiveRoot{
+			HasRole:    hasRoleDirective.Direct,
+			Pagination: pagination.Pagination,
+		},
+	}
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(config))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	return func(c *gin.Context) {
+		srv.ServeHTTP(c.Writer, c.Request)
+	}
 }
 
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// TODO: use getEnvOrDie in shared
 func getEnvOrDie(key string) string {
 	env, exists := os.LookupEnv(key)
 	if !exists {
