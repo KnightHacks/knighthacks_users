d7f3d4b5afd69aa5194e738a21eaf0334bf22b0f
dominiquedave <33738582+dominiquedave@users.noreply.github.com>
2022-06-21 15:20:35 -0400

    fixed delete user

--- repository/repository.go
+++ repository/repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+
 	"github.com/KnightHacks/knighthacks_shared/models"
 	"github.com/KnightHacks/knighthacks_users/graph/model"
 )
@@ -11,11 +12,11 @@ type Repository interface {
 	GetUserByOAuthUID(ctx context.Context, oAuthUID string, provider models.Provider) (*model.User, error)
 
 	UpdateUser(ctx context.Context, id string, input model.NewUser) (*model.User, error)
-  
-  GetOAuth(ctx context.Context, userId string) (*model.OAuth, error)
+
+	GetOAuth(ctx context.Context, userId string) (*model.OAuth, error)
 
 	GetUsers(ctx context.Context, first int, after string) ([]*model.User, int, error)
 	SearchUser(ctx context.Context, name string) ([]*model.User, error)
-
+	DeleteUser(ctx context.Context, id string) (bool, error)
 	CreateUser(ctx context.Context, oAuth *model.OAuth, input *model.NewUser) (*model.User, error)
 }
